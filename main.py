b4502871c33a3c09f0a71933e606ddae15177ea5
Jae Lee <307875+leejaew@users.noreply.github.com>
2022-12-24 14:40:20 +0000

    Add a dash between the year, month, and day.

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ import datetime
 current_date = datetime.datetime.now()
 
 # Get the user's input date
-print("Enter a date in the format YYYY-MM-DD:")
+print("Enter a date in the format YYYYMMDD:")
 user_input = input()
 
+# Add dashes to the user's input date
+formatted_date = user_input[:4] + "-" + user_input[4:6] + "-" + user_input[6:]
+
 # Parse the user's input date
-user_date = datetime.datetime.strptime(user_input, "%Y-%m-%d")
+user_date = datetime.datetime.strptime(formatted_date, "%Y-%m-%d")
 
 # Calculate the difference between the two dates
 date_difference = user_date - current_date
 
 # Print the number of days remaining
-print(f"There are {date_difference.days} days remaining until {user_input}.")
+print(f"There are {date_difference.days} days remaining until {formatted_date}.")
